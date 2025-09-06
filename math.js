20e54106e225285f4cfe3d510a1f213c287a21ab
akshaydevarakonda <akshaydevarakonda88@gmail.com>
2025-09-06 17:11:05 +0530

    akshay

---
+++ math.js
@@ -0,0 +1,21 @@
+export const add=function add(x,y)
+{
+    return x+y;
+}
+
+export const multiply=function(x,y)
+{
+    return x*y;
+}
+
+export const subtract =(x,y)=>{
+    return x-y;
+}
+
+
+const divide=(x,y)=>
+{
+    return x/y;
+}
+
+export default divide;
