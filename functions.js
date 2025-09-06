f6d5ee38b3f8b6c2cf5146a8eedb647004412011
akshaydevarakonda <akshaydevarakonda88@gmail.com>
2025-09-06 15:54:05 +0530

    akshay

---
+++ functions.js
@@ -0,0 +1,30 @@
+function greet(){
+    console.log("Welcome to Devastra")
+}
+greet()
+
+function add(x,y)
+{
+    return x+y;
+}
+let result=add(18,2)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+const multiply=function(x,y)
+{
+    return x*y;
+}
+console.log(multiply(5,3))
+
+const square =(x)=> x*x
+console.log(square(5))
+
+const sub =(x,y)=>{
+    console.log(x,y)
+    return x-y;
+}
+
+console.log(sub(10,5))
