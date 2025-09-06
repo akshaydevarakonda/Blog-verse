1d5b1e4ba7e2b3ba5d526add9c19f2fa5672b4c6
akshaydevarakonda <akshaydevarakonda88@gmail.com>
2025-09-06 12:34:03 +0530

    akshay

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("Hello World")
+
+let name = "Akshay"
+console.log(name)
+
+let num = 18
+console.log(num)
+
+let islogged = false
+console.log(islogged)
+
+let x
+console.log(x)
+
+let y=null
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr =[1,2,3,4]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"Akshay",
+ age:20
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
